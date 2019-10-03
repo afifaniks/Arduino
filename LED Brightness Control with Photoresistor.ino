2c8f182952dd160ed8462c2af4a4a8b84931fc00
Afif Al Mamun <afifanik@gmail.com>
2019-10-03 22:07:38 +0600

    Photoresistor added

---
+++ LED Brightness Control with Photoresistor.ino
@@ -0,0 +1,14 @@
+int state = 0;
+
+void setup()
+{
+    pinMode(A0, INPUT);
+    pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+    int photores_reading = analogRead(A0);
+    int mapped_value = map(photores_reading, 6, 679, 255, 0);
+    analogWrite(9, mapped_value);
+}
